ee0d79cac0ba196c0ccb5ae802a9747d813b2b47
John Clem <johnnyclem@gmail.com>
2014-06-19 11:46:42 -0700

    Initial Commit

---
+++ IBDesignable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignable
+//
+//  Created by John Clem on 6/19/14.
+//  Copyright (c) 2014 Learn Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
